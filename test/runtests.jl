c1a6eac2f7d143393fb7a5e45af02c4d1ba3998d
stensmo <141365386+stensmo@users.noreply.github.com>
2024-01-03 20:28:33 +0100

    Fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ include("setup.jl")
 @testset "Connect Test" begin
 
     @test files == actualFiles
-    @test stats == actualStructs
+    @test stats[1] == actualStructs[1]
     @test isfile(tempDir * "KeyGenerator.png")
     @test dirs == ["example"]
     @test isfile("readme.txt")
