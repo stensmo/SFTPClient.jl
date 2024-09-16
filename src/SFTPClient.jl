dda8594aa6bf870ab0021e32b6edeb9010a2e242
stensmo <141365386+stensmo@users.noreply.github.com>
2024-09-16 20:31:06 +0200

    Testing public keyword

--- src/SFTPClient.jl
+++ src/SFTPClient.jl
@@ -1,5 +1,9 @@
 module SFTPClient
 
+ @static if VERSION ≥ v"1.11"
+       public SFTP, readdir, download,upload, cd, rm, rmdir, mkdir, mv, sftpstat, SFTPStatStruct, isdir, isfile, filemode, walkdir
+end
+
 export SFTP, readdir, download,upload, cd, rm, rmdir, mkdir, mv, sftpstat, SFTPStatStruct, isdir, isfile, filemode, walkdir
 
 include("SFTPImpl.jl")
