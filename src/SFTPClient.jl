69f9c528639e9ab030d75a2a90dc4a01b2c697db
stensmo <141365386+stensmo@users.noreply.github.com>
2025-02-18 23:05:32 +0100

    Update SFTPClient.jl

--- src/SFTPClient.jl
+++ src/SFTPClient.jl
@@ -1,10 +1,8 @@
 module SFTPClient
 
- @static if VERSION ≥ v"1.11"
-  eval(Meta.parse("public SFTP, readdir, download,upload, cd, rm, rmdir, mkdir, mv, sftpstat, SFTPStatStruct, isdir, isfile, filemode, walkdir"))
-else
+
  export SFTP, readdir, download,upload, cd, rm, rmdir, mkdir, mv, sftpstat, SFTPStatStruct, isdir, isfile, filemode, walkdir
-end
+
  
 include("SFTPImpl.jl")
 
