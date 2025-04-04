0688f87dc795229bab317036746d38a71d0a811c
stensmo <141365386+stensmo@users.noreply.github.com>
2025-04-04 21:32:17 +0200

    Fixed Make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ using SFTPClient
 using Documenter, DocumenterVitepress
 
 makedocs(;sitename="Julia SFTPClient Documentation",
-    format=DocumenterVitepress.MarkdownVitepress(repo = "github.com/stensmo/SFTPClient.jl.git"),
+    format=DocumenterVitepress.MarkdownVitepress(repo = "github.com/stensmo/SFTPClient.jl"),
     )
 
+deploydocs(; repo="github.com/stensmo/SFTPClient.jl", devbranch="main", push_preview=true)
